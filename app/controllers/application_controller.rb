091de9fe8d26214f844ebab37268cd53fe6ed60e
Francisco Hui <f@franciscohui.com>
2013-08-09 13:11:37 -0400

    Reverted application controller for user permissions

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,11 +1,3 @@
 class ApplicationController < ActionController::Base
-  before_filter :configure_permitted_parameters, if: :devise_controller?
-
-  protected
-
-  def configure_permitted_parameters
-  		devise_parameter_sanitizer.for(:sign_up) { |u| u.permit(:name, :email, :password, :password_confirmation) }
-  		devise_parameter_sanitizer.for(:account_update) { |u| u.permit(:name, :email, :current_assword, :password, :password_confirmation)}
-  end
+  protect_from_forgery
 end
-
